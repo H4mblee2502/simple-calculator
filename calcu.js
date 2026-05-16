a2d969627d8adc0894ffb7d34d18670dd7d23ab7
h4mblee <bulacaners@gmail.com>
2026-05-17 05:41:38 +0800

    Initial calculator setup

---
+++ calcu.js
@@ -0,0 +1,28 @@
+
+let day = 7;
+
+if(day == 1){
+    console.log(`It's Monday!`)
+}
+else if(day == 2){
+    console.log(`It's Tuesday!`)
+}
+else if(day == 3){
+    console.log(`It's Wednesday!`)
+}
+else if(day == 4){
+    console.log(`It's Thursday!`)
+}
+else if(day == 5){
+    console.log(`It's Friday!`)
+}
+else if(day == 6){
+    console.log(`It's Saturdaa!`)
+}
+else if(day == 7){
+    console.log(`It's Sunday!`)
+}
+else{
+    console.log(`${day} is not a day`)
+}
+
